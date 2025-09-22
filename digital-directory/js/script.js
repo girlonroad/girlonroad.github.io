7b9c9b6a9d0782a26b1161ae1a8de3f8b16d3317
Girl On Road <92860547+girlonroad@users.noreply.github.com>
2025-09-22 10:34:23 +1000

    Add files via upload

---
+++ digital-directory/js/script.js
@@ -0,0 +1,23 @@
+const about = document.getElementById("about-button");
+const popup = document.getElementById("popup");
+const x = document.getElementById("x");
+
+about.addEventListener("click", () => {
+  popup.className = "";
+});
+
+x.addEventListener("click", () => {
+  popup.className = "hidden";
+});
+
+const about = document.getElementById("index-button");
+const popup = document.getElementById("popup");
+const x = document.getElementById("x");
+
+about.addEventListener("click", () => {
+  popup.className = "";
+});
+
+x.addEventListener("click", () => {
+  popup.className = "hidden";
+});
